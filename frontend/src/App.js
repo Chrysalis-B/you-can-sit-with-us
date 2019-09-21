de6c3d551854ab6639f8c8536b35f4505edad609
Heini Ahokas <ahokas.heini@gmail.com>
2019-09-21 14:19:22 +0200

    Add hardcoded list of unis and add basic routing to front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import CreateCampaignForm from "./create-campaign-form";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
-  return <p>Hello World</p>;
+  return (
+    <div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/create">Create</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/create" exact component={CreateCampaignForm} />
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
