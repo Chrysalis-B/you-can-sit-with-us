f40d15e9a94932d43b7144b012329a7a8b566bce
Heini Ahokas <ahokas.heini@gmail.com>
2019-09-21 12:58:09 +0200

    Add mongo connection and update read me

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,23 @@
-import express from "express";
+import express from 'express';
+import { MongoClient } from 'mongodb';
 
 const port = 3001;
-
 const app = express();
+const databaseUrl = 'mongodb://localhost:27017/groupDiscountManager';
 
-app.get("/", (req, res) => res.send("Hello World"));
+MongoClient.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(client => {
+    app.locals.db = client.db('groupDiscountManager');
 
-app.listen(port, () => console.log("App is listening on port " + port));
+  })
+  .catch(() => console.error('Failed to connect to the database')
+);
+
+
+
+app.get("/", (req, res) => res.send('Hello World'));
+
+app.listen(port, () => console.log('App is listening on port ' + port));
 
 const larp = async () => {
   return 0;
